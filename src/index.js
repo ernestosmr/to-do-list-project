f1283b2bcee10b277b0a0f53fdb89ac8be813145
Ernesto Mendoza <ernestogbh@gmail.com>
2022-05-10 16:26:18 -0500

    new delete btn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 
 const mainSection = document.getElementById('taskContainer')
-let taskNumber = 0;
-const inputCheckBox = []
 function errorInput(){
     const errorDiv = document.getElementById('div-error')
     errorDiv.classList.remove('invisible')
@@ -16,22 +14,29 @@ function testResults (form){
     if(testInput === '' || testDate === ''){
         return errorInput()
     }
-    taskNumber += 1;
     const article = document.createElement('article')
     const title = document.createElement('h2')
     const p = document.createElement('p')
+    const deletebtn = document.createElement('button')
     const inputCheck = document.createElement('input')
     inputCheck.type = 'checkbox'
     inputCheck.setAttribute('id', 'inputCheck')
     article.classList.add('task-contain')
-    article.setAttribute('id', taskNumber)
     article.appendChild(title)
     article.appendChild(p)
     article.appendChild(inputCheck)
+    article.appendChild(deletebtn)
     title.textContent = testInput
     p.textContent = testDate
+    deletebtn.textContent = '🗑️'
+    deletebtn.setAttribute('class', 'delete-btn')
     mainSection.appendChild(article)
-    inputCheckBox.push(inputCheck)
+    deletebtn.addEventListener('click', () => {
+        article.style.animationName = 'delete'
+        setTimeout(() => {
+            article.classList.add('invisible')
+        }, 1000)
+    })
     inputCheck.addEventListener('click', () => {
         if(inputCheck.checked){
             article.classList.add('checked')
